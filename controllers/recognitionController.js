66cade2b67175236d9e78e0c6dfd604c1fc5df46
SupriyaGembali <94470209+SupriyaGembali@users.noreply.github.com>
2022-05-21 21:39:12 +0530

    Updates recognition.js

--- controllers/recognitionController.js
+++ controllers/recognitionController.js
@@ -1,1 +1,17 @@
+module.exports = function(app){
 
+
+    app.get('/index',function(req,res){
+       
+    });
+
+    app.post('/index',function(req,res){
+
+    });
+
+    app.delete('/index',function(req,res){
+
+    });
+
+
+};
