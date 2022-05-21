17d05cdbd0ef89a5c708c5f0c4b616d6e3f8985e
SupriyaGembali <94470209+SupriyaGembali@users.noreply.github.com>
2022-05-21 22:00:57 +0530

    Add files via upload

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,1 +1,52 @@
+const imageUpload = document.getElementById('imageUpload')
 
+Promise.all([
+  faceapi.nets.faceRecognitionNet.loadFromUri('./assets/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('./assets/models'),
+  faceapi.nets.ssdMobilenetv1.loadFromUri('./assets/models')
+]).then(start)
+
+async function start() {
+  const container = document.createElement('div')
+  container.style.position = 'relative'
+  document.body.append(container)
+  const labeledFaceDescriptors = await loadLabeledImages()
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.8)
+  let image
+  let canvas
+  document.body.append('Loaded')
+  imageUpload.addEventListener('change', async () => {
+    if (image) image.remove()
+    if (canvas) canvas.remove()
+    image = await faceapi.bufferToImage(imageUpload.files[0])
+    container.append(image)
+    canvas = faceapi.createCanvasFromMedia(image)
+    container.append(canvas)
+    const displaySize = { width: image.width, height: image.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+    results.forEach((result, i) => {
+      const box = resizedDetections[i].detection.box
+      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+      drawBox.draw(canvas)
+    })
+  })
+}
+
+function loadLabeledImages() {
+  const labels = ['gopi', 'hema', 'pranay', 'pujitha', 'siddu', 'solanki', 'koti']
+  return Promise.all(
+    labels.map(async label => {
+      const descriptions = []
+      for (let i = 1; i <= 2; i++) {
+        const img = await faceapi.fetchImage(`./assets/labeled_images/${label}/${i}.jpg`)
+        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+        descriptions.push(detections.descriptor)
+      }
+
+      return new faceapi.LabeledFaceDescriptors(label, descriptions)
+    })
+  )
+}
