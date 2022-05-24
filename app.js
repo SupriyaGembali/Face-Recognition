2272f975ab4416f732ad9caeb401fba2ea4b0077
SupriyaGembali <94470209+SupriyaGembali@users.noreply.github.com>
2022-05-24 21:39:12 +0530

    Added google authentication code

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express=require('express');
 var recognitionController = require('./controllers/recognitionController');
-var app=express();
-
-
+const app = express();
 //set up template engine
+
+
 app.set('view engine','ejs');
 
 //static files
@@ -11,6 +11,52 @@ app.use(express.static('./public'));
 
 //fire controller
 recognitionController(app);
+
+const session = require('express-session');
+const passport = require('passport');
+require('./auth');
+
+
+
+function isLoggedIn(req, res, next) {
+  req.user ? next() : res.sendStatus(401);
+}
+
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', (req, res) => {
+  res.send('<a href="/auth/google">Authenticate with Google</a>');
+});
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: [ 'email', 'profile' ] }
+));
+
+app.get( '/auth/google/callback',
+  passport.authenticate( 'google', {
+    successRedirect: '/protected',
+    failureRedirect: '/auth/google/failure'
+  })
+);
+
+app.get('/protected', isLoggedIn, (req, res) => {
+  res.render('index');
+});
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.send('Goodbye!');
+});
+
+app.get('/auth/google/failure', (req, res) => {
+  res.send('Failed to authenticate..');
+});
+
+
+
 
 
 //listen to port
