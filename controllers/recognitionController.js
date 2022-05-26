3ecaca21a50a8276bdc819c011c65f22361ed656
SupriyaGembali <94470209+SupriyaGembali@users.noreply.github.com>
2022-05-26 19:26:35 +0530

    Removed unwanted code

--- controllers/recognitionController.js
+++ controllers/recognitionController.js
@@ -1,28 +1,17 @@
 module.exports = function(app){
 
-
+    //index route
     app.get('/index',function(req,res){
         res.render('index');
     });
-
+    
+    //home route
     app.get('/home',function(req,res){
         res.render('home');
     });
 
+    //contact route
     app.get('/contact',function(req,res){
         res.render('contact');
     });
-
-    
-
-
-    app.post('/index',function(req,res){
-
-    });
-
-    app.delete('/index',function(req,res){
-
-    });
-
-
 };
